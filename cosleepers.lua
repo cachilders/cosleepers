1ebb262cfd38ef259e5820c84bf6e93cc69c7b7d
Casey A. Childers <cachilders@gmail.com>
2024-02-16 21:47:11 -0800

    [chore] boilerplate

---
+++ cosleepers.lua
@@ -0,0 +1,21 @@
+-- cosleepers
+-- a family of four in a california king
+
+engine.name = 'cosleepers'
+
+-- params
+
+function init()
+end
+
+function enc(enc, delta)
+end
+
+function key(key, z)
+end
+
+function redraw()
+end
+
+function refresh()
+end
